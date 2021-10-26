3cca4dbf4ff4b5e96b03f96e99ff2ce6e468bf5b
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-26 11:42:22 +0100

    Store Application

--- src/main/java/com/ecommerce/store/web/ProductController.java
+++ src/main/java/com/ecommerce/store/web/ProductController.java
@@ -3,7 +3,9 @@ package com.ecommerce.store.web;
 import com.ecommerce.store.data.model.Product;
 import com.ecommerce.store.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,8 +40,14 @@ public class ProductController {
         return ResponseEntity.ok("Product deleted successfully");
     }
 
-    @PostMapping("/update/{id}")
-    public ResponseEntity<?> update(@RequestBody Product product){
+    @GetMapping("/find/{id}")
+    public ResponseEntity<?> findProductById(@PathVariable Long id){
+        productServiceImpl.findById(id);
+        return ResponseEntity.ok(HttpStatus.ACCEPTED);
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<?> update(@RequestBody Product product, @PathVariable Long id){
         log.info("Product request --> {}", product);
         productServiceImpl.updateProduct(product, product.getId());
         return ResponseEntity.ok("Product updated successfully");
