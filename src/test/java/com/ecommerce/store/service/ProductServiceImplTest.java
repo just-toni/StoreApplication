bb3678d36a6c8e4e9d84e008b91e3c699e636186
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-26 11:35:56 +0100

    Store Application

--- src/test/java/com/ecommerce/store/service/ProductServiceImplTest.java
+++ src/test/java/com/ecommerce/store/service/ProductServiceImplTest.java
@@ -8,6 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -27,9 +30,34 @@ class ProductServiceImplTest {
     @Test
     void saveProductMockTest() {
         Product product = new Product();
-        when(productServiceImpl.save(product)).thenReturn(product);
+        when(productRepository.save(product)).thenReturn(product);
         productServiceImpl.save(product);
         verify(productRepository, times(1)).save(product);
+    }
+
+    @Test
+    void findProductByIdMockTest(){
+        Product product = new Product();
+        product.setName("Prototype");
+        product.setId(19L);
+        when(productRepository.findById(19L)).thenReturn(Optional.of(product));
+        Product product1 = productServiceImpl.findById(product.getId());
+        verify(productRepository, times(1)).findById(19L);
+        assertThat(product1).isNotNull();
+        assertThat(product1.getId()).isEqualTo(product.getId());
+        assertThat(product1.getName()).isEqualTo(product.getName());
+    }
+    
+    @Test
+    void updateProductMockTest(){
+        Product product = new Product();
+        product.setName("Dummy product");
+        product.setId(20L);
+        when(productRepository.findById(10L)).thenReturn(Optional.of(product));
+        productServiceImpl.updateProduct(product, 10L);
+        verify(productRepository, times(1)).save(product);
+        assertEquals("Dummy product", product.getName());
+        assertThat("Dummy product").isEqualTo(product.getName());
     }
 
 }
